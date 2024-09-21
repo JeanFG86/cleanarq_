0b0d5378d5c1dd92aac14d5c84e22218e67b1ece
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-09-21 13:40:44 -0300

    create Account test

---
+++ backend/ride/test/Account.test.ts
@@ -0,0 +1,32 @@
+import Account from "../src/Account";
+
+describe("Account test", () => {
+  it("Deve criar uma conta", () => {
+    const account = Account.create("Jhon Doe", "41953697038", "jhon.doe@gmail.com", "", "123456", true, false);
+    expect(account).toBeDefined();
+  });
+
+  it("Não deve criar uma conta com nome inválido", () => {
+    expect(() => Account.create("Jhon", "41953697038", "jhon.doe@gmail.com", "", "123456", true, false)).toThrow(
+      new Error("Invalid name")
+    );
+  });
+
+  it("Não deve criar uma conta com email inválido", () => {
+    expect(() => Account.create("Jhon Doe", "41953697038", "jhon.doegmail.com", "", "123456", true, false)).toThrow(
+      new Error("Invalid email")
+    );
+  });
+
+  it("Não deve criar uma conta com cpf inválido", () => {
+    expect(() => Account.create("Jhon Doe", "4195367038", "jhon.doe@gmail.com", "", "123456", true, false)).toThrow(
+      new Error("Invalid cpf")
+    );
+  });
+
+  it("Não deve criar uma conta com placa do carro inválida", () => {
+    expect(() => Account.create("Jhon Doe", "41953697038", "jhon.doe@gmail.com", "", "123456", true, true)).toThrow(
+      new Error("Invalid car plate")
+    );
+  });
+});
